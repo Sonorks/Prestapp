2960d1d188f6ebc8f3780b50b6e563940f7c3c24
sonorks <julivas96@gmail.com>
2017-05-04 09:26:08 -0500

    Javadoc en todas las clases java

--- src/com/edu/udea/prestapp/dao/InterfaceUsuarioDao.java
+++ src/com/edu/udea/prestapp/dao/InterfaceUsuarioDao.java
@@ -1,5 +1,12 @@
 package com.edu.udea.prestapp.dao;
 
+/**
+ * @author Cristian Berrio - cbp453252.hdrl@gmail.com
+ * @author Julian Vasquez - julivas96@gmail.com
+ * @author David Acevedo - davida.acevedo@udea.edu.co
+ * @version = 1.0
+ */
+
 import java.util.List;
 
 import com.edu.udea.prestapp.dto.Usuario;
